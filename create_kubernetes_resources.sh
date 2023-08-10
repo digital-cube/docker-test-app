beb1710f95a093428bc3cf5da81436b08504b005
acast83 <acast83@gmail.com>
2023-08-10 15:38:29 +0200

    wip

--- create_kubernetes_resources.sh
+++ create_kubernetes_resources.sh
@@ -4,9 +4,8 @@ set -e
 kubectl apply -f kubernetes/db-deployment.yaml
 kubectl apply -f kubernetes/db-service.yaml
 
-kubectl apply -f kubernetes/frontend-deployment.yaml
-kubectl apply -f kubernetes/frontend-service.yaml
-
 kubectl apply -f kubernetes/app-deployment.yaml
 kubectl apply -f kubernetes/app-service.yaml
 
+kubectl apply -f kubernetes/frontend-deployment.yaml
+kubectl apply -f kubernetes/frontend-service.yaml
