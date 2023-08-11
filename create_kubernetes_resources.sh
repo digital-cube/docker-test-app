b84b84b088b6d461f83456c08ab9a5ed7ee2cd34
acast83 <acast83@gmail.com>
2023-08-11 22:10:02 +0200

    wip

--- create_kubernetes_resources.sh
+++ create_kubernetes_resources.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+kubectl delete secret secret-envs --ignore-not-found
+kubectl create secret generic secret-envs --from-env-file=.env
+
 kubectl apply -f kubernetes/db-deployment.yaml
 kubectl apply -f kubernetes/db-service.yaml
 
--- delete_kubernetes_resources.sh
+++ delete_kubernetes_resources.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-kubectl delete -f kubernetes/db-deployment.yaml
-kubectl delete -f kubernetes/db-service.yaml
+kubectl delete secret secret-envs --ignore-not-found
 
-kubectl delete -f kubernetes/frontend-deployment.yaml
-kubectl delete -f kubernetes/frontend-service.yaml
+kubectl delete -f kubernetes/db-deployment.yaml --ignore-not-found
+kubectl delete -f kubernetes/db-service.yaml --ignore-not-found
 
-kubectl delete -f kubernetes/app-deployment.yaml
-kubectl delete -f kubernetes/app-service.yaml
+kubectl delete -f kubernetes/frontend-deployment.yaml --ignore-not-found
+kubectl delete -f kubernetes/frontend-service.yaml --ignore-not-found
+
+kubectl delete -f kubernetes/app-deployment.yaml --ignore-not-found
+kubectl delete -f kubernetes/app-service.yaml --ignore-not-found
 
